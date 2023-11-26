d3c6d31c9714e5387aa1cbaf54868208ea3477ae
gusemil <gusemil@hotmail.com>
2023-11-26 20:17:02 +0100

    Treasure coins started and more general functions

--- Program.cs
+++ Program.cs
@@ -12,10 +12,10 @@ class Program
         Console.WriteLine("Hello Castle Keeper!");
         while (mainLoop)
         {
-            Console.WriteLine("Do you want to choose or roll treasure level? Y/N?");
+            Console.WriteLine("Do you want to choose or roll treasure level? C/R");
             int treasureLevel;
             ConsoleKeyInfo input = Console.ReadKey();
-            if (input.Key == ConsoleKey.Y)
+            if (input.Key == ConsoleKey.C)
             {
                 Console.WriteLine("Choose a number between 1-18");
                 string treasureLevelInput = Console.ReadLine();
@@ -23,17 +23,18 @@ class Program
                 if (treasureLevelInputInt > 0 && treasureLevelInputInt <= 18)
                 {
                     treasureLevel = treasureLevelInputInt;
+                    RollTreasure(treasureLevel);
                 }
                 else
                 {
                     InvalidInput();
                 }
-
             }
-            else if (input.Key == ConsoleKey.N)
+            else if (input.Key == ConsoleKey.R)
             {
                 Console.WriteLine("Vittu");
                 treasureLevel = RollNumber(1,18);
+                RollTreasure(treasureLevel);
             }
             else
             {
@@ -41,21 +42,107 @@ class Program
             }
 
             //End
-            Console.WriteLine("Do you wish to end the program? Y/N");
+            Console.WriteLine("Do you wish to end the application? Y/N");
             if(Console.ReadKey().Key == ConsoleKey.Y) { break; }
         }
     }
 
+    #region General Methods
     public static void InvalidInput()
     {
         Console.WriteLine("Invalid input. Please try again");
     }
 
-    public static int RollNumber(int min, int max)
+    public static int RollNumber(int min, int max, int modifier = 0)
     {
         Random rnd = new Random();
         int roll = rnd.Next(min, max);
-        Console.WriteLine("Rolling " + min + "-" + max + " Result: " + roll);
+        if(modifier > 0)
+        {
+            roll += modifier;
+        }
+        Console.WriteLine("Rolling " + min + "-" + max + " + modifier: " + modifier + " Result: " + roll);
+
         return roll;
     }
+
+    public static int RollXdY(int x, int y, int modifier = 0, int multiplier = 0)
+    {
+        int roll = 0;
+        Random rnd = new Random();
+        for (int i = 0; i < x; i++)
+        {
+            roll += rnd.Next(1, y);
+        }
+
+        if (modifier > 0)
+        {
+            roll += modifier;
+        }
+
+        if(multiplier > 0)
+        {
+            roll *= multiplier;
+        }
+
+        return roll;
+    }
+
+    public static int RollPercentage()
+    {
+        Random rnd = new Random();
+        int roll = rnd.Next(1,100);
+        return roll;
+    }
+
+    public static bool RollPercentageSuccess(int percentageChance)
+    {
+        bool result = true;
+        int roll = RollPercentage();
+        if (percentageChance > 0 && roll > percentageChance)
+        {
+            result = false;
+        }
+        return result;
+    }
+    #endregion
+
+    //Main Rolls
+    #region MainTreasureRolls
+    private static void RollTreasure(int treasureLevel)
+    {
+        RollCoins(treasureLevel);
+        //RollGems(treasureLevel);
+        //RollExtraOrdinaryItems(treasureLevel);
+        //RollMagicItems(treasureLevel);
+    }
+
+    private static void RollCoins(int treasureLevel)
+    {
+        if(treasureLevel == 1)
+        {
+            bool coinChance = RollPercentageSuccess(50);
+            int coins = 0;
+            if (coinChance)
+            {
+                coins = RollXdY(2, 4, 0, 10);
+            }
+            Console.WriteLine("The treasure contains " + coins + " gold pieces");
+        }
+    }
+
+    private static void RollGems(int treasureLevel)
+    {
+
+    }
+    private static void RollMagicItems(int treasureLevel)
+    {
+
+    }
+
+    private static void RollExtraOrdinaryItems(int treasureLevel)
+    {
+
+    }
+    #endregion
 }
