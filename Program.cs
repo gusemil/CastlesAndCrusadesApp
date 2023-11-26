5b190a907270c5c483703e097abf78470fc8e5cd
gusemil <gusemil@hotmail.com>
2023-11-26 18:01:13 +0100

    Treasure Level roll or  choose

--- Program.cs
+++ Program.cs
@@ -4,13 +4,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CastlesAndCrusadesApp
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        bool mainLoop = true;
+        Console.WriteLine("Hello Castle Keeper!");
+        while (mainLoop)
         {
-            Console.WriteLine("Hello Castle Keeper!");
+            Console.WriteLine("Do you want to choose or roll treasure level? Y/N?");
+            int treasureLevel;
+            ConsoleKeyInfo input = Console.ReadKey();
+            if (input.Key == ConsoleKey.Y)
+            {
+                Console.WriteLine("Choose a number between 1-18");
+                string treasureLevelInput = Console.ReadLine();
+                int treasureLevelInputInt = Convert.ToInt32(treasureLevelInput);
+                if (treasureLevelInputInt > 0 && treasureLevelInputInt <= 18)
+                {
+                    treasureLevel = treasureLevelInputInt;
+                }
+                else
+                {
+                    InvalidInput();
+                }
+
+            }
+            else if (input.Key == ConsoleKey.N)
+            {
+                Console.WriteLine("Vittu");
+                treasureLevel = RollNumber(1,18);
+            }
+            else
+            {
+                InvalidInput();
+            }
+
+            //End
+            Console.WriteLine("Do you wish to end the program? Y/N");
+            if(Console.ReadKey().Key == ConsoleKey.Y) { break; }
         }
+    }
+
+    public static void InvalidInput()
+    {
+        Console.WriteLine("Invalid input. Please try again");
+    }
+
+    public static int RollNumber(int min, int max)
+    {
+        Random rnd = new Random();
+        int roll = rnd.Next(min, max);
+        Console.WriteLine("Rolling " + min + "-" + max + " Result: " + roll);
+        return roll;
     }
 }
